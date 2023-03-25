6cd490c3a2d1d70659b67464763728568a82da14
Max Mearsheimer <max@seemaxwork.com>
2023-03-24 20:08:06 -0500

    Home CSS

--- partials/_gradient-back.php
+++ partials/_gradient-back.php
@@ -1,24 +1,40 @@
 <div id="background-svg-callout" class="fixed-back">
-  <svg id="background-svg-callout-gradient-1" preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 1945 1649.96">
-    <defs>
-    <style>
-    .cls-1{fill:url(#linear-gradient);}</style>
-    <linearGradient id="linear-gradient" x1="293.89" y1="-1467.38" x2="1651.11" y2="-254.74" gradientTransform="translate(0 -36.08) scale(1 -1)" gradientUnits="userSpaceOnUse">
-    <stop offset="0" stop-color="#bdd9f9"/>
-    <stop offset="1" stop-color="#bdd9f9" stop-opacity="0"/>
-    </linearGradient>
-    </defs>
-    <path class="cls-1" d="m972.5,1649.96c537.1,0,972.5-369.36,972.5-824.98S1509.6,0,972.5,0,0,369.36,0,824.98s435.4,824.98,972.5,824.98Z"/>
-  </svg>
-  <svg id="background-svg-callout-gradient-2" preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 1830 1552.57">
-    <defs>
-    <style>
-    .cls-2{fill:url(#linear-gradient-2);}</style>
-    <linearGradient id="linear-gradient-2" x1="1553.54" y1="1334.18" x2="276.45" y2="193.25" gradientTransform="translate(0 1540) scale(1 -1)" gradientUnits="userSpaceOnUse">
-    <stop offset="0" stop-color="#ffe6d9"/>
-    <stop offset="1" stop-color="#ffe6d9" stop-opacity="0"/>
-    </linearGradient>
-    </defs>
-    <path class="cls-2" d="m915,1552.57c505.34,0,915-347.55,915-776.28S1420.34,0,915,0,0,347.56,0,776.29s409.66,776.28,915,776.28Z"/>
-  </svg>
+  <div id="background-svg-callout-gradient-1">
+    <svg preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+      viewBox="0 0 1945 1649.96">
+      <defs>
+        <style>
+        .cls-1 {
+          fill: url(#linear-gradient);
+        }
+        </style>
+        <linearGradient id="linear-gradient" x1="293.89" y1="-1467.38" x2="1651.11" y2="-254.74"
+          gradientTransform="translate(0 -36.08) scale(1 -1)" gradientUnits="userSpaceOnUse">
+          <stop offset="0" stop-color="#bdd9f9" />
+          <stop offset="1" stop-color="#bdd9f9" stop-opacity="0" />
+        </linearGradient>
+      </defs>
+      <path class="cls-1"
+        d="m972.5,1649.96c537.1,0,972.5-369.36,972.5-824.98S1509.6,0,972.5,0,0,369.36,0,824.98s435.4,824.98,972.5,824.98Z" />
+    </svg>
+  </div>
+  <div id="background-svg-callout-gradient-2">
+    <svg preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+      viewBox="0 0 1830 1552.57">
+      <defs>
+        <style>
+        .cls-2 {
+          fill: url(#linear-gradient-2);
+        }
+        </style>
+        <linearGradient id="linear-gradient-2" x1="1553.54" y1="1334.18" x2="276.45" y2="193.25"
+          gradientTransform="translate(0 1540) scale(1 -1)" gradientUnits="userSpaceOnUse">
+          <stop offset="0" stop-color="#ffe6d9" />
+          <stop offset="1" stop-color="#ffe6d9" stop-opacity="0" />
+        </linearGradient>
+      </defs>
+      <path class="cls-2"
+        d="m915,1552.57c505.34,0,915-347.55,915-776.28S1420.34,0,915,0,0,347.56,0,776.29s409.66,776.28,915,776.28Z" />
+    </svg>
+  </div>
 </div>
